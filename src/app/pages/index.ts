8dfab8e195f0163a90bf212a2951252f384c44a8
liuzheng <liuzheng712@gmail.com>
2021-12-11 20:39:23 +0800

    feat: update

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -4,18 +4,15 @@ import {Routes, RouterModule} from "@angular/router";
 import {PageIndexComponent} from "./index/index.component"
 import {PageBlankComponent} from "./blank/blank.component";
 import {PageNotFoundComponent} from "./not-found/not-found.component";
-import {PageJumpComponent} from "./jump/jump.component";
 
 import {PartModule} from "../parts";
 import {AppRootComponent} from "./root/root.component";
 import {PluginModule} from "../plugins";
 
 export {AppRootComponent} from "./root/root.component"
-export {PageJumpComponent} from "./jump/jump.component";
 
 export const PageRoutes: Routes = [
   {path: "undefined", component: PageBlankComponent},
-  {path: "jump", component: PageJumpComponent},
   {path: "", component: PageIndexComponent},
   {path: "**", component: PageNotFoundComponent}
 ];
@@ -30,7 +27,6 @@ export const PageRoutes: Routes = [
     PageIndexComponent,
     PageBlankComponent,
     PageNotFoundComponent,
-    PageJumpComponent
   ],
 })
 export class PageModule {
