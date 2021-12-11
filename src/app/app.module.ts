c47c1e8d7199ea2e2dcb6f22f517835c3c7e3c8e
liuzheng <liuzheng712@gmail.com>
2021-12-11 20:56:54 +0800

    feat: update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms"; // <-- NgModel lives here
+import {FormsModule} from "@angular/forms"; // <-- NgModel lives here
 import {HttpClientModule} from "@angular/common/http";
 
 import {AppRoutingModule} from "./app-routing.module";
@@ -16,7 +16,6 @@ import {PartModule} from "./parts";
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
     AppRoutingModule,
     PipesModule,
     PluginModule,
--- src/app/plugins/MaterialModule.component.ts
+++ src/app/plugins/MaterialModule.component.ts
@@ -37,8 +37,7 @@ import {MatTabsModule} from "@angular/material/tabs";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatTooltipModule} from "@angular/material/tooltip";
 import {MatTreeModule} from "@angular/material/tree";
-
-
+import {ReactiveFormsModule} from "@angular/forms";
 
 class icon {
   name: string | undefined;
@@ -47,6 +46,7 @@ class icon {
 
 @NgModule({
   exports: [
+    ReactiveFormsModule,
     CdkTableModule,
     MatAutocompleteModule,
     MatBadgeModule,
