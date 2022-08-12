2460da1be79552371a32d16b83b99f5894787183
Liu Zheng <liuzheng712@gmail.com>
2022-08-12 22:24:32 +0800

    feat: update

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,9 +1,7 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 
-import {AppRootComponent} from "./root/root.component";
 import {PluginModule} from "../plugins";
-import { Globals } from "../globals";
 
 export {AppRootComponent} from "./root/root.component"
 
