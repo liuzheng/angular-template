ac1edd19ff1191ae637aeeca73215154a3ba4440
liuzheng <liuzheng712@gmail.com>
2024-07-28 21:24:28 +0800

    update

--- src/app/plugins/terminal/terminal.component.ts
+++ src/app/plugins/terminal/terminal.component.ts
@@ -1,8 +1,8 @@
 import {Component, ElementRef, OnInit, AfterViewInit, ViewChild} from "@angular/core";
-import {Terminal} from "xterm";
-import {WebLinksAddon} from "xterm-addon-web-links";
-import {AttachAddon} from "xterm-addon-attach";
-import {FitAddon} from "xterm-addon-fit";
+import {Terminal} from "@xterm/xterm";
+import {WebLinksAddon} from "@xterm/addon-web-links";
+import {AttachAddon} from "@xterm/addon-attach";
+import {FitAddon} from "@xterm/addon-fit";
 import "zmodem.js/dist/zmodem.devel"
 
 // todo: zmode support
